e520bf1f0c3ea53144c3ddc31cfbddd00b7bb210
chkalov12 <chkalov007@gmail.com>
2020-05-14 00:28:14 +0300

    change lend

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,15 @@
 $(function(){
-	$('.header__btn').click(function(){
-		$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 1000);
-		return false;
-    });
+	//  $('.header__btn').click(function(){
+	//  	$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 15000);
+	//  	return false;
+  //    });
+
+
+     $('.header__btn').click(function(e){
+      e.preventDefault();
+      $('body, html').animate({ scrollTop: $('.form').offset().top }, 10000);
+      });
+
     
     $('.burger-btn').on('click', function(){
         $('.menu__list').slideToggle();
